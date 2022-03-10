bc17f7deaf2cb18741eccd7df6b67bd308efd697
chancel simo <chancel.simo@nextalys.com>
2022-03-10 19:22:02 +0100

    readme

--- Controllers/movie.controller.ts
+++ Controllers/movie.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiConsumes, ApiProduces, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { MovieDto } from "../dto/movie.dto";
 import { MovieService } from "../Services/movie.service";
 
+@ApiConsumes('application/json')
+@ApiProduces('application/json')
 @ApiTags('movie')
 @Controller('movie')
 export class MovieController {
@@ -12,21 +14,29 @@ export class MovieController {
 
     }
 
+    @ApiResponse({ status: 200, description: 'movies succeed', })
+    @ApiResponse({ status: 404, description: 'ressource absente' })
     @Get()
     getMovies() {
         return this.movieService.getMovies();
     }
 
+    @ApiResponse({ status: 200, description: 'movies succeed', })
+    @ApiResponse({ status: 404, description: 'ressource absente' })
     @Get('/:id')
     getMovie(@Param() id: string) {
         return this.movieService.getMovie(id);
     }
 
+    @ApiResponse({ status: 201, description: 'movies add succeed', })
+    @ApiResponse({ status: 422, description: 'validation impossible' })
     @Post()
     addMovie(@Body() movie: MovieDto) {
         return this.movieService.createMovie(movie);
     }
 
+    @ApiResponse({ status: 201, description: 'movies add succeed', })
+    @ApiResponse({ status: 422, description: 'validation impossible' })
     @Put('/update')
     updateMovie(@Body() movie: MovieDto) {
         return this.movieService.updateMovie(movie);
